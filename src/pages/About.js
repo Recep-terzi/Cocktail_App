e2cb099b3d90519c297945368b2bbc440d141162
Recep Terzi <recepterzi-67@hotmail.com>
2022-03-29 13:21:03 +0300

    Cocktail_App

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title"> about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eveniet
+        dolore architecto excepturi sit molestias accusantium nemo earum in
+        laboriosam iste reprehenderit id dolorum reiciendis sequi provident,
+        esse recusandae sed?
+      </p>
+    </section>
+  );
+}
+
+export default About;
